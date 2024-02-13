4f81c405b4a5783cc86b2173e788c20c30c25c74
Кочерженко Игорь Васильевич <i.kocherzhenko@keyauto.ru>
2024-02-13 13:08:05 +0300

    init

---
+++ src/main/resources/db/migration/V01__init_db.sql
@@ -0,0 +1,46 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.3
+-- Dumped by pg_dump version 9.6.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: demo; Type: DATABASE; Schema: -; Owner: -
+--
+drop database if exists demo;
+CREATE DATABASE demo;
+
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: bookings; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA bookings;
+
+
+--
+-- Name: SCHEMA bookings; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON SCHEMA bookings IS 'Airlines demo database schema';
+
